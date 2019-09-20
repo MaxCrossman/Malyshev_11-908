2048d944f1f6d9fe0a3d9691e75130e128b5decd
MaxCrossman <skrillex.bahta.12@mail.ru>
2019-09-20 18:35:38 +0300

    first

---
+++ Hello world.java
@@ -0,0 +1,28 @@
+
+
+package lesson_2
+
+//public private protected
+
+/*
+ *
+ * Olegsey
+*
+ */
+
+/*
+ * boolean: true/false
+ * целочисленные:
+ * byte short long int chat
+ * вещественные :
+ * flat
+ */
+
+public class HelloWorld [
+public static main(String [] arps)[
+        System.out.println("Hello, World!");
+
+        System.out.print("LOL");
+
+        ]
+        ]
