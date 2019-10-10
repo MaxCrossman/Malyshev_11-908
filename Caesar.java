aaaa6419c7b2f998994b326ab2c8dbb55fc12be1
MaxCrossman <skrillex.bahta.12@mail.ru>
2019-10-10 10:18:06 +0300

    Homework update 2.0

--- Caesar.java
+++ Caesar.java
@@ -6,19 +6,19 @@ public class Caesar{
 		System.out.println("Enter your message");
 		String m = sc.next();
 		System.out.println("Enter the number");
-		int n = sc.nextInt();
+		int n = (sc.nextInt())%26;
 		String abc = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
 		if (n < 0) {
 			for (int i = 0; i < m.length(); i++) {
 			char l = m.charAt(i);
 			int t = abc.lastIndexOf(l);
-			System.out.print(abc.charAt(t+(n%26)));
+			System.out.print(abc.charAt(t+n));
 			}
 		} else {
 		for (int i = 0; i < m.length(); i++) {
 			char l = m.charAt(i);
 			int t = abc.indexOf(l);
-			System.out.print(abc.charAt(t+(n%26)));
+			System.out.print(abc.charAt(t+n));
 			}
 		}
 	}
