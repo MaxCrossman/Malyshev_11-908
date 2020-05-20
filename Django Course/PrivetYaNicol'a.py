73d8ee2e6bc256c6d4a8e244b86c1d05e8c8bc3f
MaxCrossman <skrillex.bahta.12@mail.ru>
2020-05-20 19:44:05 +0300

    Django|Abramskiy

---
+++ Django Course/PrivetYaNicol'a.py
@@ -0,0 +1,4 @@
+input('Write anything to get information...')
+print('\n\n\n\nMAXIM MALYSHEV aka Max Crossman\n','School: ITIS','Programming language: Java(~1 year), Python(3 mins)','Other skills: Sound Design(1.5 year), Sound Engineering(3 years), Music production(4 years)',sep='\n')
+print('\n\n\n')
+input('Write anything to exit the programm')
---
+++ Django Course/PrivetYaNicola.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PrivetYaNicola {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write anything to get information...");
+        sc.next();
+        System.out.println("\n\n\n\nMAXIM MALYSHEV aka Max Crossman\n");
+        System.out.println("School: ITIS");
+        System.out.println("Programming language: Java(1 year)");
+        System.out.println("Other skills: Sound Design(1.5 year), Sound Engineering(3 years)," +
+                " Music production(4 years)");
+    }
+}
